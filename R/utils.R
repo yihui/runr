d436e7d9c342447044c1511d66ffa7149dead0ad
Yihui Xie <xie@yihui.name>
2013-07-31 17:54:15 -0700

    let these engines return objects of class runr_results

--- R/utils.R
+++ R/utils.R
@@ -10,7 +10,26 @@ split_results = function(x, sep) {
   res = vector('list', length(x))
   for (i in seq_along(x)) {
     el = gsub('^\n+|\n+$', '', x[i])
-    res[[i]] = if (i %% 2 == 1) structure(list(src = el), class = 'source') else el
+    res[[i]] = if (i %% 2 == 1) new_source(el) else paste(el, collapse = '\n')
   }
-  res
+  structure(res, class = c('runr_results', 'list'))
+}
+
+# construct a list of results of the class runr_results
+new_results = function(src, out) {
+  structure(list(new_source(src), paste(out, collapse = '\n')),
+            class = c('runr_results', 'list'))
+}
+
+new_source = function(src) structure(list(src = src), class = 'source')
+
+#' @S3method print runr_results
+print.runr_results = function(x, comment = '# ') {
+  for (i in seq_along(x)) {
+    is.src = inherits(x[[i]], 'source')
+    el = if (is.src) x[[i]][['src']] else x[[i]]
+    if (length(el) == 0 || identical(el, '')) next
+    if (!is.src) el = sprintf('%s%s', comment, el)
+    cat(el, sep = '\n')
+  }
 }
